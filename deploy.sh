a4a499bde43e4048db90ff803bcb53504542c241
Xinyu Zhou <zxytim@gmail.com>
2017-08-07 15:13:57 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p server_log
-gunicorn -w 3 run_demo_server:app -b 0.0.0.0 -p 8769 -t 120 \
-	--capture-output \
+gunicorn -w 3 run_demo_server:app -b 0.0.0.0:8769 -t 120 \
 	--error-logfile server_log/error.log \
 	--access-logfile server_log/access.log
