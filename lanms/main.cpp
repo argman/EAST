8a8f994b7106bdf6eac221efb6eb73144f56373f
burak-yildizoz <45322678+burak-yildizoz@users.noreply.github.com>
2021-02-18 20:13:18 +0300

    lanms main does not depend on adaptor.cpp
    merge_quadrangle_n9 takes datavec.size()/9 as n

--- lanms/main.cpp
+++ lanms/main.cpp
@@ -7,7 +7,7 @@ int main()
     const float data[] = {0, 0, 0, 1, 1, 1, 1, 0, 1};
     std::vector<float> datavec(data, data + sizeof(data)/sizeof(float));
 
-    std::vector<lanms::Polygon> polys = lanms::merge_quadrangle_n9(datavec.data(), datavec.size());
+    std::vector<lanms::Polygon> polys = lanms::merge_quadrangle_n9(datavec.data(), datavec.size() / 9);
     std::cout << polys.size() << std::endl;
 
     return 0;
